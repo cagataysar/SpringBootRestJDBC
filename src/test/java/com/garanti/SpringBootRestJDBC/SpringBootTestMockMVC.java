e0fe629a84b7a990f828eed68237bb65e773fcc9
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2023-01-13 19:50:05 +0300

    new commit

--- src/test/java/com/garanti/SpringBootRestJDBC/SpringBootTestMockMVC.java
+++ src/test/java/com/garanti/SpringBootRestJDBC/SpringBootTestMockMVC.java
@@ -1,9 +1,13 @@
 package com.garanti.SpringBootRestJDBC;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.garanti.SpringBootRestJDBC.model.Ogretmen;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -12,11 +16,27 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 // AutoConfigureMockMvc yazmazsam aşağıdaki değişkeni inject edemez
-@AutoConfigureMockMvc // mvc uygualmam ayakta imişçenise
+@AutoConfigureMockMvc // mvc uygulamam ayaktaymış gibi çalışıyor
 public class SpringBootTestMockMVC
 {
     @Autowired
     public MockMvc mock;
+
+    @Test
+    public void saveOgretmen() throws Exception
+    {
+        String ogretmen = new ObjectMapper().writeValueAsString(new Ogretmen("junit", true));
+        System.err.println("----> " + ogretmen);
+        RequestBuilder request = MockMvcRequestBuilders.post("/ogretmen/save").content(ogretmen).contentType(MediaType.APPLICATION_JSON).header("Accept-Language", "en");
+        MockHttpServletResponse response = mock.perform(request).andReturn().getResponse();
+        // response.getStatus() == 201
+        // response.getContentAsString() == "Successfully saved"
+        // javanın dafault keyword ü
+        // assert response.getContentAsString().equals("Successfully saved");
+        // org.assertj.core.api.Assertions
+        org.junit.jupiter.api.Assertions.assertEquals("Successfully saved", response.getContentAsString());
+        System.err.println("------> " + response.getContentAsString());
+    }
 
     @Test
     public void findByName() throws Exception
