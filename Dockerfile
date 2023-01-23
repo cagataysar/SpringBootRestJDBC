ab3348c4ee578735519d9b0e31f9a4ec8bb9842b
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2023-01-23 10:17:14 +0300

    new updates

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,8 @@ EXPOSE 9090
 ARG JAR_FILE=target/SpringBootRestJDBC-1.0.0.jar
 ADD ${JAR_FILE} application.jar
 ENTRYPOINT ["java", "-jar", "/application.jar"]
+
+# commands
+# docker build -t appname .
+# docker image ls
+# docker run -p8080:8080 ***
--- src/main/java/com/garanti/SpringBootRestJDBC/model/Role.java
+++ src/main/java/com/garanti/SpringBootRestJDBC/model/Role.java
@@ -3,20 +3,19 @@ package com.garanti.SpringBootRestJDBC.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
 
+//TODO: Service package'ına UserDetails sınıfı eklenecek
 @Setter
 @Getter
 @AllArgsConstructor
-public class Role implements GrantedAuthority
+public class Role implements org.springframework.security.core.GrantedAuthority
 {
     private static final long serialVersionUID = 9156064156119386503L;
 
     private String name;
 
     @Override
-    public String getAuthority()
-    {
+    public String getAuthority() {
         return name;
     }
 }
